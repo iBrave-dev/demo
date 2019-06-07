3dc8b81a11e15e4d9c0aa4c2e9e917cb689f64d7
iBrave-dev <50228505+iBrave-dev@users.noreply.github.com>
2019-06-07 23:23:07 +0300

    commit

---
+++ demo.dart
@@ -0,0 +1,34 @@
+class ProductBloc extends Bloc<ProductEvent, ProductState> {
+  final ProductModel productModel;
+  ProductBloc({@required this.productModel}) :assert(productModel != null);
+ @override
+  Stream<ProductState> mapEventToState(
+    ProductEvent event,
+  ) async* {
+  try{
+      if(event is AddProductToWishList)
+        yield* _addProductToWishListMTS();
+      else if(event is AddProductToCart)
+        yield* _addProductToCartMTS(event.cartItem);
+      else if(event is ChangeProductState)
+        yield* _changeProductStateMTS();
+ }catch(e){
+     yield ErrorState():
+} }
+Stream _addProductToWishListMTS()async*{
+    //Api 
+     await AiRepository().addToWishList(productModel);
+  }
+}
+
+//Snippet of ApiRepository like class
+
+class ApiRepository{
+
+  Future<void> addToWishList(ProductModel productModel)async{
+      //Does some HTTPCall that may throw error if something happens
+      throw Error();
+  }
+
+}
+
